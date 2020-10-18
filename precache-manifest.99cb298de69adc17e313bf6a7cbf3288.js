09f0b9c414b464ab0a5235ba7c5c7545c2aa8b02
GATI Saif <ser.contact.sgit@gmail.com>
2020-10-19 00:19:39 +0100

    Updates

---
+++ precache-manifest.99cb298de69adc17e313bf6a7cbf3288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2941406ceb26c28b80b2a821cfde9390",
+    "url": "/monsters-react-app/index.html"
+  },
+  {
+    "revision": "2bae8c315cad30f4fb6b",
+    "url": "/monsters-react-app/static/css/main.a5cff86e.chunk.css"
+  },
+  {
+    "revision": "78f96b92dc0c6c461866",
+    "url": "/monsters-react-app/static/js/2.ad8eb8ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-react-app/static/js/2.ad8eb8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bae8c315cad30f4fb6b",
+    "url": "/monsters-react-app/static/js/main.1b2a0343.chunk.js"
+  },
+  {
+    "revision": "3c7d3bb3d619f4d7d3c1",
+    "url": "/monsters-react-app/static/js/runtime-main.c74710eb.js"
+  }
+]);
